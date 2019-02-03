a7828c91fbe80ce2bb46130588ebf8c2af190494
Eric Wilsey <ericwilsey@hotmail.com>
2019-02-03 00:06:43 -0700

    update to script and index.

--- HW-3/script.js
+++ HW-3/script.js
@@ -2,6 +2,11 @@ function doSomething()
 {
     window.alert("Welcome to Armageddon!");
     document.write("<h3>Never frenchfry when it's time to pizza.</h3>");
+    document.write("As a self-aware potato, your only mission is to survive. Simple?
+    Many obstacles stand in your path. Many foes stand to gain noteriety
+  from your defeat. Will you become the baked potato you were destined to
+become? Or will you transform into something greater? The choice is yours,
+but...");
 }
 
 doSomething();
