1a3c7ce1d247b9dadb115f59623800050610bb91
Eric Wilsey <ericwilsey@hotmail.com>
2019-02-02 18:28:37 -0700

    Created HW-2, readme, script.js, et index.html

---
+++ HW-3/script.js
@@ -0,0 +1,13 @@
+/*
+  Author: Eric Wilsey
+  Date: Feb 2nd, 2019
+  Purpose of this script: Values - Data types.
+*/
+
+var favoriteRestaurant = "Good Burger";
+var favotireNumber = 12;
+var otherVariable;
+
+console.write(favoriteRestaurant);
+console.write(favotireNumber);
+console.write(otherVariable);
