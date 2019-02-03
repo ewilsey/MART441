6d79a185e12a607f7ac5efb97e03a3960ecd3970
Eric Wilsey <ericwilsey@hotmail.com>
2019-02-02 19:13:43 -0700

    Update script.js

--- HW-3/script.js
+++ HW-3/script.js
@@ -8,7 +8,7 @@ var favoriteRestaurant = "Good Burger\nRestaurant";
 var favotireNumber = 12%3;
 var otherVariable = null;
 var isAlive = true;
-favoriteRestaurant = "Real Hooman Cuisine\nRestaurant";
+var secondNumber = "Real Hooman Cuisine\nRestaurant";
 
 console.log(favoriteRestaurant);
 console.log(favotireNumber);
