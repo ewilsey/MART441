2e63c760c1fbee2428b454d3e38af0ff22687382
Eric Wilsey <ericwilsey@hotmail.com>
2019-02-03 01:27:28 -0700

    Driving myself right round, baby right round, like a record.

--- HW-3/script.js
+++ HW-3/script.js
@@ -4,3 +4,6 @@ function doSomething()
     document.write("<h1>Never frenchfry when it's time to pizza.</h1>");
     document.write("<h3>As a self-aware potato, your only mission is to survive. Simple? Many obstacles stand in your path. Many foes stand to gain noteriety from your defeat. Will you become the baked potato you were once destined to become? </h3>");
     document.write("<h3>Or will you rise above and transform into something much greater? The choice is yours, what will you do?</h3>");
+
+
+doSomething();
