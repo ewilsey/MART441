2e43acaa4b4bcf36fad7f6f20ecb2202eada7e98
Eric Wilsey <ericwilsey@hotmail.com>
2019-02-03 01:32:36 -0700

    Update script.js

--- HW-3/script.js
+++ HW-3/script.js
@@ -5,5 +5,5 @@ function doSomething()
     document.write("<h3>As a self-aware potato, your only mission is to survive. Simple? Many obstacles stand in your path. Many foes stand to gain noteriety from your defeat. Will you become the baked potato you were once destined to become? </h3>");
     document.write("<h3>Or will you rise above and transform into something much greater? The choice is yours, what will you do?</h3>");
 
-
+}
 doSomething();
