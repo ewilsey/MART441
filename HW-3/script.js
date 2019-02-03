60f43388cbb8d6fced092b1b1280e603afdc341c
Eric Wilsey <ericwilsey@hotmail.com>
2019-02-03 00:12:13 -0700

    Update script.js

--- HW-3/script.js
+++ HW-3/script.js
@@ -2,11 +2,7 @@ function doSomething()
 {
     window.alert("Welcome to Armageddon!");
     document.write("<h3>Never frenchfry when it's time to pizza.</h3>");
-    document.write("As a self-aware potato, your only mission is to survive. Simple?
-    Many obstacles stand in your path. Many foes stand to gain noteriety
-  from your defeat. Will you become the baked potato you were destined to
-become? Or will you transform into something greater? The choice is yours,
-but...");
+    document.write(){"<h6>As a self-aware potato, your only mission is to survive. Simple?Many obstacles stand in your path. Many foes stand to gain noterietyfrom your defeat. Will you become the baked potato you were destined tobecome? Or will you transform into something greater? The choice is yours, but will you...</h6>";
 }
 
 doSomething();
