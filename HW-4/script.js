32ab9ca3e09dd86538685e6b7dbb241ccfe81885
Eric Wilsey <ericwilsey@hotmail.com>
2019-02-10 17:12:05 -0700

    Update script.js

--- HW-4/script.js
+++ HW-4/script.js
@@ -1,4 +1,13 @@
-function getChoice1(){
-  var userChoice = document.getElementById("choice").value;
-  console.log(userChoice);
+function getChoice1() {
+  var userChoice = document.getElementById('choice').value;
+  var gameProposal = document.getElementById('proposal');
+  if(userChoice === "Stay")  {
+    gameProposal.innerHTML = "Proposal1";
+  }
+  else if (userChoice ==="Go") {
+    gameProposal.innerHTML = "Proposal2";
+  }
+  else {
+    gameProposal.innerHTML = "Invalid Response";
+  }
 }
