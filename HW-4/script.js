07ad4a23836a0bc9e5db248ade61b74eebaf96cb
Eric Wilsey <ericwilsey@hotmail.com>
2019-02-10 19:18:27 -0700

    Update script.js

--- HW-4/script.js
+++ HW-4/script.js
@@ -1,4 +1,13 @@
 function getChoice1() {
   var myChoice = document.getElementById('choice').value;
-  console.log(myChoice);
+  var myQuestion = document.getElementById('question');
+  if(myChoice ==="Stay","stay") {
+    myQuestion.innerHTML = "Have you listened to nothing I have told you? You have surely sealed your fate.";
+  }
+  else if(myChoice ==="Go","go")  {
+    myQuestion.innerHTML = "The first step forward is the fist step towards success.";
+  }
+  else {
+    myQuestion.innerHTML = "Ivalid Answer"
+  }
 }
