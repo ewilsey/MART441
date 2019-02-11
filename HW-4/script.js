dc2afe132f68f1bb1de1db2c27b275b04896b927
Eric Wilsey <ericwilsey@hotmail.com>
2019-02-10 19:39:14 -0700

    Update script.js

--- HW-4/script.js
+++ HW-4/script.js
@@ -2,10 +2,10 @@ function getChoice1() {
   var myChoice = document.getElementById('choice').value;
   var myQuestion = document.getElementById('question');
   if(myChoice === "Stay") {
-    myQuestion.innerHTML = "Have you listened to nothing I have told you? You have surely sealed your fate.";
+    myQuestion.innerHTML = "Have you listened to nothing I have told you? You have surely sealed your fate. Would you like to play again?";
   }
   else if(myChoice === "Go")  {
-    myQuestion.innerHTML = "The first step forward is the fist step towards success.";
+    myQuestion.innerHTML = "The first step forward is the fist step towards success and your first step out of the door and into a brand new world. You see a large parking lot. Will you turn RIGHT and head down the dark alley? Or will you turn LEFT and heads towards the bustop?";
   }
   else {
     myQuestion.innerHTML = "Invalid Answer";
