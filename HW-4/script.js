aaab3d8041cbe33b523eefea345fb269eec68f78
Eric Wilsey <ericwilsey@hotmail.com>
2019-02-10 22:00:37 -0700

    Update script.js

--- HW-4/script.js
+++ HW-4/script.js
@@ -14,7 +14,14 @@ function getChoice1() {
   else if(myChoice === "Go")  {
     myQuestion.innerHTML = "The first step forward is the fist step towards success and your first step out of the door and into a brand new world. You see a large parking lot. Will you turn RIGHT and head down the dark alley? Or will you turn LEFT and heads towards the bustop? The choice is yours: Right or Left?";
 
+    document.getElementById('choice').style.display="none";
+    document.getElementById('btnSubmit').style.display="none";
 
+    document.getElementById('choice2').style.display="none";
+    document.getElementById('btnSubmit2').style.display="none";
+
+    document.getElementById('choice3').style.display="block";
+    document.getElementById('btnSubmit3').style.display="block";
 
   }
   else {
