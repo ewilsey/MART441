015a49b8b464502f8a1ea7b2c11ab48d6bd88c30
Eric Wilsey <ericwilsey@hotmail.com>
2019-02-16 21:41:55 -0700

    Update script.js

--- HW-5/script.js
+++ HW-5/script.js
@@ -27,13 +27,13 @@ function createRandomImgArray()
     // create another array to make sure the images only get added twice
     var count = [0,0];
     // create a while statement to check to see if our actual image array is full
-    while(actualImgs.length < 4)
+    while(actualImgs.length < 10)
     {
         // get a random number between 0 and the number total number of images that we can choose from
         var randomNumber = Math.floor(Math.random() * actualImgPath.length)
           // create an if statement that says if the total number added is less than 2, then
         // add the image to the actual image array
-        if(count[randomNumber] < 2)
+        if(count[randomNumber] < 5)
         {
             actualImgs.push(actualImgPath[randomNumber]);
             // then add one to the array that makes sure only two images can be added
