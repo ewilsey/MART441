7edeceac1c66a05fbc3fcebd7994d87974a595c5
Eric Wilsey <ericwilsey@hotmail.com>
2019-03-04 19:42:42 -0700

    Update script.js

--- HW-7/script.js
+++ HW-7/script.js
@@ -13,7 +13,7 @@ get theTitle() {
 
 function  initializeArray() {
   var myVFinder = new vFinder("Title 1 Info");
-  var myVFinder = new vFinder("Title 2 Info");
+  var myVFinder1 = new vFinder("Title 2 Info");
   myVFinderArray.push(myVFinder);
   myVFinderArray.push(myVFinder1);
 }
