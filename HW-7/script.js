2979407c8c9a9acc6b81c519ef6ea38ba1fece32
Eric Wilsey <ericwilsey@hotmail.com>
2019-03-04 20:48:52 -0700

    Update

--- HW-7/script.js
+++ HW-7/script.js
@@ -14,8 +14,14 @@ get theTitle() {
 function  initializeArray() {
   var myVFinder = new vFinder("Title 1 Info");
   var myVFinder1 = new vFinder("Title 2 Info");
+  var myVFinder2 = new vFinder("Title 3 Info");
+  var myVFinder3 = new vFinder("Title 4 Info");
+  var myVFinder4 = new vFinder("Title 5 Info");
   myVFinderArray.push(myVFinder);
   myVFinderArray.push(myVFinder1);
+  myVFinderArray.push(myVFinder2);
+  myVFinderArray.push(myVFinder3);
+  myVFinderArray.push(myVFinder4);
 }
 
 function  accessInfo() {
