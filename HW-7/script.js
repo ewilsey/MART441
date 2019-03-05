3612b43721e268ed2bdf0c70856b779116aee9c4
Eric Wilsey <ericwilsey@hotmail.com>
2019-03-04 17:37:55 -0700

    Update script.js

--- HW-7/script.js
+++ HW-7/script.js
@@ -5,10 +5,17 @@ class VFO {
 
 
 toString() {
-  return "Title: " + this.title; 
+  return "Title: " + this.title;
 }
 get title() {
     return  this.title;
 }
+
+}
+
+function  accessInfo() {
+  var myVFinder = new VF("Title 1 Info");
+  console.log(myVFinder.toString());
+  console.log(myVFinder.title);
 
 }
