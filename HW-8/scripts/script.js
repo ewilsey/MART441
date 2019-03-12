8f1822a35e60ece1555cd5df5769746c1cb505a8
Eric Wilsey <ericwilsey@hotmail.com>
2019-03-11 20:38:04 -0600

    Update script.js

--- HW-8/scripts/script.js
+++ HW-8/scripts/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $("button").click(function(){
-    $("#eyeL").animate({left:250}).animate({left:150}).animate({left:200});
-    $("#eyeR").animate({left:750}).animate({left:650}).animate({left:700});
+    moveEyes();
   });
 });
+
+function moveEyes() {
+  $("#eyeL").animate({left:250}).animate({left:150}).animate({left:200});
+  $("#eyeR").animate({left:750}).animate({left:650}).animate({left:700});
+}
