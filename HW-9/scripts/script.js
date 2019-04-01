7bbf722b0fe85c1df979b9363cfd4a767620e8e3
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-01 14:23:23 -0600

    changes

--- HW-9/scripts/script.js
+++ HW-9/scripts/script.js
@@ -1,29 +1,10 @@
 let tractor =  {
-  {
     "Manufacturer": "Fendt",
     "Model": "828 Vario",
     "Color": "Green",
     "Year": "2010"
-  },
-  {
-    "Manufacturer": "Schlüter",
-    "Model": "1500 TVL",
-    "Color": "Red",
-    "Year":"1981"
-  },
-  {
-    "Manufacturer": "Case IH",
-    "Model": "Magnum CVX",
-    "Color": "Red",
-    "Year":"1987"
-  },
-  {
-    "Manufacturer": "New Holland",
-    "Model": "T5",
-    "Color": "Blue",
-    "Year":"2017"
   }
-}
+
 
 function showTractorInfo()
 {
