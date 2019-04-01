579098f019f79b748e5acb63a5f26ada45fc3044
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-01 01:18:12 -0600

    Update main.js

--- HW-9/js/main.js
+++ HW-9/js/main.js
@@ -1,6 +1,6 @@
 var grabRequest = new XMLHttpRequest();
-grabRequest.open('Get', 'https://ewilsey.github.io/MART441/HW-9/json/tractos.json')
-grabRequest.onload = function(){
+grabRequest.open('GET', 'https://ewilsey.github.io/MART441/HW-9/json/tractos.json')
+grabRequest.onload = function() {
   console.log(grabRequest.responseText);
 };
 grabRequest.send();
