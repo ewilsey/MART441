6765dd68a53e1905fa967f94124412d9c033514a
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-01 00:53:51 -0600

    created tractor json

--- HW-9/js/main.js
+++ HW-9/js/main.js
@@ -1,7 +1,27 @@
- var myTractor = {
-   "Make": "Fendt",
-   "Model": "828 Vario",
-   "Color": "Black"
- }
 
- myTractor.Color
+ var theTractors = [
+   {
+     "Make": "Fendt",
+     "Model": "828 Vario",
+     "Color": "Green",
+     "Years": "2010 -"
+   },
+   {
+     "Make": "Schlüter",
+     "Model": "1500 TVL",
+     "Color": "Red",
+     "Years":"1981-1984"
+   },
+   {
+     "Make": "Case IH",
+     "Model": "Magnum CVX",
+     "Color": "Red",
+     "Years":"1987-"
+   },
+   {
+     "Make": "New Holland",
+     "Model": "T5",
+     "Color": "Blue",
+     "Years":"2017"
+   }
+ ]
