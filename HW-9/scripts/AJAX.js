811a28f443d8a93de885c19a6b1a63e1208c6a03
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-01 22:53:31 -0600

    button should change json page by +1 for every click

--- HW-9/scripts/AJAX.js
+++ HW-9/scripts/AJAX.js
@@ -2,13 +2,14 @@ var tractorCounter = 1;
 
 $(document).ready(function () {
   $("button").click(function () {
-    $("#tractorInformation").load("data/tractor-2.json", function(responseText) {
+    $("#tractorInformation").load("data/tractor-" + tractorCounter + ".json", function(responseText) {
       var tractor = JSON.parse(responseText);
       $("#tractorInformation").html("Manufacturer: " + tractor.manufacturer
       + "<br>Model: " + tractor.model + "<br> Body Color: " + tractor.color.body + "<br> Rim Color: "
       + tractor.color.rim + "<br>Available Wheel Setups:<br>" +
       tractor.setups[0] + "<br>" + tractor.setups[1] + "<br>" + tractor.setups[2] + "<br>" + tractor.setups[3]);
     });
+    tractorCounter ++;
   });
 });
 
