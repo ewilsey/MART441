cf943db2f28d718b8656baa743ce5f4d81a43816
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-01 21:22:00 -0600

    17:59

--- HW-9/scripts/AJAX.js
+++ HW-9/scripts/AJAX.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
   $("button").click(function () {
-    $("#tractorInformation").load("data/tractor.json", fadeText);
+    $("#tractorInformation").load("data/tractor.json", function(responseText) {
+      alert(responseText);
+    });
   });
 });
 
-function fadeText()
+/*function fadeText()
 {
   $("#tractorInformation").fadeOut("slow").fadeIn("slow");
 }
+*/
