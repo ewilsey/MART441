3b9cbd07442600122aad703e265f8ec26b84b7d2
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-01 21:33:59 -0600

    Update AJAX.js

--- HW-9/scripts/AJAX.js
+++ HW-9/scripts/AJAX.js
@@ -2,7 +2,10 @@ $(document).ready(function () {
   $("button").click(function () {
     $("#tractorInformation").load("data/tractor.json", function(responseText) {
       var tractor = JSON.parse(responseText);
-      alert(tractor.manufacturer);
+      $("#tractorInformation").html("Manufacturer: " + tractor.manufacturer
+      + "<br>Model: " + tractor.model + "<br> Body Color: " + tractor.color.body + "<br> Rim Color: "
+      + tractor.color.rim + "<br>Available Wheel Setups:<br>" +
+      tractor.setups[0] + "<br>" + tractor.setups[1] + "<br>" + tractor.setups[2] + "<br>" + tractor.setups[3]);
     });
   });
 });
