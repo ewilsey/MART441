f31e6234277f4ac4f7a80518bc4d332f399e0cf1
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-01 21:31:55 -0600

    Update AJAX.js

--- HW-9/scripts/AJAX.js
+++ HW-9/scripts/AJAX.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $("button").click(function () {
     $("#tractorInformation").load("data/tractor.json", function(responseText) {
       var tractor = JSON.parse(responseText);
+      alert(tractor.manufacturer);
     });
   });
 });
