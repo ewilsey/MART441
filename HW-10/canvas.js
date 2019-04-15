f97bd76a424f2af8542247a1e51ff093af0e40a4
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-15 03:58:00 -0600

    4:06

--- HW-10/canvas.js
+++ HW-10/canvas.js
@@ -1,5 +1,19 @@
 var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var x = 10;
+var y = 10;
+var w = 10;
+var h = 10;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.fillRect(x,y,w,h);
+ctx.fillRect(x + 20 ,y + 10,w,h);
+ctx.fillRect(x + 40 ,y + 20,w,h);
+
+//line
+ctx.beginPath();
+ctx.moveTo(x, y);
+ctx.lineTo(x + 40, y + 20);
+ctx.stroke();
 console.log(canvas);
