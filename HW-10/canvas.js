cccf05572e8c1e271324012c5029a26b0a0b5d72
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-15 04:50:30 -0600

    9:17

--- HW-10/canvas.js
+++ HW-10/canvas.js
@@ -1,22 +1,31 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var x = 10;
-var y = 10;
-var w = 10;
-var h = 10;
+var x = 300;
+var y = 300;
+var w = 300;
+var h = 300;
+var r = 30;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.fillStyle = 'rgba(255,0,0,0.25)';
 ctx.fillRect(x,y,w,h);
+ctx.fillStyle = 'rgba(0,255,0,0.25)';
 ctx.fillRect(x + 20 ,y + 10,w,h);
+ctx.fillStyle = 'rgba(0,0,255,0.25)';
 ctx.fillRect(x + 40 ,y + 20,w,h);
 
 //line
 ctx.beginPath();
 ctx.moveTo(x, y);
 ctx.lineTo(x + 40, y + 20);
-ctx.strokeStyle = 'red'; //must be above stroke
+ctx.strokeStyle = 'yellow'; //must be above stroke
 ctx.stroke();
 
-ctx.closePath();
+
+//circle //arc
+ctx.beginPath();
+ctx.arc(x,y,r,0,Math.PI * 2, false);
+ctx.strokeStyle = 'cyan';
+   ctx.stroke();
 console.log(canvas);
