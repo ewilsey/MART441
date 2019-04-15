619ab4bca8bf1c298858b21f4838397c521ec560
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-14 22:57:02 -0600

    Why is my script not working? Why ? I don't get it!!!

--- HW-10/scripts/main.js
+++ HW-10/scripts/main.js
@@ -1,15 +1,4 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var x = 50;
-var y = 50;
-ctx.fillStyle = "#0000FF";
-drawSquare();
-setInterval(update, 1000);
-
-function update(){
-drawSquare();
-}
-
-function drawSquare(){
-ctx.fillRect(x, y, 20, 20);
-}
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, 80, 100);
