c31cfcf06b8f0017efcc49e9497c461d015b725d
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-19 22:54:45 -0600

    test ::3

--- HW-10/scripts/canvas.js
+++ HW-10/scripts/canvas.js
@@ -14,38 +14,48 @@ canvas.height = window.innerHeight;
 
 
 //rect
-c.fillStyle = 'rgba(255,255,0,0.5)';
-c.fillRect(x,y,w,h);
-c.fillStyle = 'rgba(0,255,255,0.5)';
-c.fillRect(x-j,y+j,w,h);
-c.fillStyle = 'rgba(255,0,255,0.5)';
-c.fillRect(x+j,y+j,w,h);
-c.fillStyle = 'rgba(0,255,0,0.5)';
-c.fillRect(x,y+k,w,h);
+//c.fillStyle = 'rgba(255,255,0,0.5)';
+//c.fillRect(x,y,w,h);
+//c.fillStyle = 'rgba(0,255,255,0.5)';
+//c.fillRect(x-j,y+j,w,h);
+//c.fillStyle = 'rgba(255,0,255,0.5)';
+//c.fillRect(x+j,y+j,w,h);
+//c.fillStyle = 'rgba(0,255,0,0.5)';
+//c.fillRect(x,y+k,w,h);
 
 
 //line
-c.beginPath();
-c.moveTo(50,300);
-c.lineTo(300,100);
-c.lineTo(400,100);
-c.strokeStyle = "blue";
-c.stroke();
+//c.beginPath();
+//c.moveTo(50,300);
+//c.lineTo(300,100);
+//c.lineTo(400,100);
+//c.strokeStyle = "blue";
+//c.stroke();
 
 //arc
 //c.arc(x:Int, y: Int, r: Int, startAngle:
 //Float, endAngle: Float,
 //drawCounterClockwise: Bool (false));
+//c.beginPath();
+//c.arc(300,300,30,0, Math.PI * 2, false);
+//c.strokeStyle = 'red';
+//c.stroke();
+
+//for (var i = 0; i < 10; i++){
+//  var x = Math.random() * window.innerWidth;
+//  var y = Math.random() * window.innerHeight;
+//  c.beginPath();
+//  c.arc(x,y,30,0, Math.PI * 2, false);
+//  c.strokeStyle = color[Math.floor(Math.random() * color.length)];
+//  c.stroke();
+//}
+
 c.beginPath();
-c.arc(300,300,30,0, Math.PI * 2, false);
-c.strokeStyle = 'red';
+c.arc(x,y,30,0, Math.PI * 2, false);
+c.strokeStyle = color[Math.floor(Math.random() * color.length)];
 c.stroke();
 
-for (var i = 0; i < 10; i++){
-  var x = Math.random() * window.innerWidth;
-  var y = Math.random() * window.innerHeight;
-  c.beginPath();
-  c.arc(x,y,30,0, Math.PI * 2, false);
-  c.strokeStyle = color[Math.floor(Math.random() * color.length)];
-  c.stroke();
+function animate(){
+  requestAnimationFrame(animate);
+  console.log('shit');
 }
