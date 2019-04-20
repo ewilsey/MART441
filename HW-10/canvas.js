3d28a62709e7de0a5e37135ef62879ca54cd5da7
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-19 21:31:51 -0600

    1:01 :: 3

--- HW-10/canvas.js
+++ HW-10/canvas.js
@@ -1,31 +1,31 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+//var canvas = document.getElementById('canvas');
+//var ctx = canvas.getContext('2d');
 //var x = 300;
 //var y = 300;
-var w = 300;
-var h = 300;
-var r = 30;
-var dx = (Math.random() - 0.5) * 8;
-var dy = (Math.random() - 0.5) * 8;
-var x = Math.random() * window.innerWidth;
-var y = Math.random() * window.innerHeight;
-var color=['cyan','royalblue','darkred','darkorange','teal','sandybrown','deepskyblue'];
+//var w = 300;
+//var h = 300;
+//var r = 30;
+//var dx = (Math.random() - 0.5) * 8;
+//var dy = (Math.random() - 0.5) * 8;
+//var x = Math.random() * window.innerWidth;
+//var y = Math.random() * window.innerHeight;
+//var color=['cyan','royalblue','darkred','darkorange','teal','sandybrown','deepskyblue'];
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ctx.fillStyle = 'rgba(255,0,0,0.25)';
-ctx.fillRect(x,y,w,h);
-ctx.fillStyle = 'rgba(0,255,0,0.25)';
-ctx.fillRect(x + 20 ,y + 10,w,h);
-ctx.fillStyle = 'rgba(0,0,255,0.25)';
-ctx.fillRect(x + 40 ,y + 20,w,h);
+//canvas.width = window.innerWidth;
+//canvas.height = window.innerHeight;
+//ctx.fillStyle = 'rgba(255,0,0,0.25)';
+//ctx.fillRect(x,y,w,h);
+//ctx.fillStyle = 'rgba(0,255,0,0.25)';
+//ctx.fillRect(x + 20 ,y + 10,w,h);
+//ctx.fillStyle = 'rgba(0,0,255,0.25)';
+//ctx.fillRect(x + 40 ,y + 20,w,h);
 
 //line
-ctx.beginPath();
-ctx.moveTo(x, y);
-ctx.lineTo(x + 40, y + 20);
-ctx.strokeStyle = 'yellow'; //must be above stroke
-ctx.stroke();
+//ctx.beginPath();
+//ctx.moveTo(x, y);
+//ctx.lineTo(x + 40, y + 20);
+//ctx.strokeStyle = 'yellow'; //must be above stroke
+//ctx.stroke();
 
 
 //circle //arc
@@ -44,46 +44,27 @@ ctx.stroke();
   ctx.stroke();
 }
 */
-console.log(canvas);
+//console.log(canvas);
 
-function Circle(x, y){
-  this.x = x;
-  this.y = y;
-  this.draw = function(){
-    ctx.beginPath();
-    ctx.arc(x,y,r,0,Math.PI * 2, false);
-    ctx.strokeStyle = 'cyan';
-    ctx.stroke();
-  }
-}
+//function  animate(){
+  //requestAnimationFrame(animate);
+  //ctx.clearRect(0,0,innerWidth,innerHeight);
+  //ctx.beginPath();
+  //ctx.arc(x,y,r,0,Math.PI * 2, false);
+  //ctx.strokeStyle = 'blue';
+  //ctx.stroke();
 
-var circle = Circle(200,200);
+  //if (x + r > innerWidth || x - r < 0) {
+    //dx =- dx;
+  //}
 
-function  animate(){
-  requestAnimationFrame(animate);
+  //if (y + r > innerHeight || y - r < 0) {
+    //dy =- dy;
 
-  ctx.clearRect(0,0,innerWidth,innerHeight);
+  //}
+//x += dx;
+//y += dy;
 
+//}
 
-
-  ctx.beginPath();
-  ctx.arc(x,y,r,0,Math.PI * 2, false);
-  ctx.strokeStyle = 'blue';
-  ctx.stroke();
-
-
-
-  if (x + r > innerWidth || x - r < 0) {
-    dx =- dx;
-  }
-
-  if (y + r > innerHeight || y - r < 0) {
-    dy =- dy;
-
-  }
-x += dx;
-y += dy;
-
-}
-
-animate();
+//animate();
