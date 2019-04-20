967d1fdcd4e947875356dd8e97c013ebf6828e8b
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-19 22:38:01 -0600

    stage complete ::2

--- HW-10/scripts/canvas.js
+++ HW-10/scripts/canvas.js
@@ -39,3 +39,12 @@ c.beginPath();
 c.arc(300,300,30,0, Math.PI * 2, false);
 c.strokeStyle = 'red';
 c.stroke();
+
+for (var i = 0; i < 10; i++){
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+  c.beginPath();
+  c.arc(x,y,30,0, Math.PI * 2, false);
+  c.strokeStyle = 'red';
+  c.stroke();
+}
