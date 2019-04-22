89ea04e881ad813b7abca372ec8342cd71ea958a
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-21 23:55:06 -0600

    Update canvas.js

--- HW-11/scripts/canvas.js
+++ HW-11/scripts/canvas.js
@@ -1,36 +1,12 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext('2d');
+window.onload = canfunc;
+window.onresize = canfun;
 
-canvas.style.width = canvas.width + "px";
-canvas.style.height = canvas.width + "px";
-input.offset = new Vector2(GetLeft(canvas), GetTop(canvas));
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
 
-var rect = new Rectangle(250,250,50,50);
-rect.color = new Color(0,0,255,1);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-var player = new Rectangle(0,0,25,25);
-player.color.g=0;
-player.color.b=0;
-
-
-var Update = setInterval(function() {
-  if (input.a)
-  player.x -= 5;
-  if (input.d)
-  player.x += 5;
-  if (input.w)
-  player.y -= 5;
-  if (input.s)
-  player.y += 5;
-  if(player.x<0)
-  player.x =0;
-  if (player.y<0)
-  player.y=0;
-}, 1);
-
-var Draw = setInterval(function() {
-
-ctx.clearRect(0,0,canvas.width,canvas.height);
-rect.Draw(ctx);
-player.Draw(ctx);
-}, 33);
+function canfunc(){
+  console.log('func');
+}
