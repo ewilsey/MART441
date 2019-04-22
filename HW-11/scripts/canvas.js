f57c1cf5768fe95cb6b444f988fb21009f35ad33
Eric Wilsey <ericwilsey@hotmail.com>
2019-04-21 23:21:05 -0600

    Update canvas.js

--- HW-11/scripts/canvas.js
+++ HW-11/scripts/canvas.js
@@ -1,5 +1,30 @@
-var canvas = document.querySelector('canvas');
-var c = canvas.getContext('2d');
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.style.width = canvas.width + "px";
+canvas.style.height = canvas.width + "px";
+input.offset = new Vector2(GetLeft(canvas), GetTop(canvas));
+
+var rect = new Rectangle(250,250,50,50);
+rect.color = new Color(0,0,255,1);
+
+var player = new Rectangle(0,0,25,25);
+player.color.g=0;
+player.color.b=0;
+
+
+var Update = setInterval(function() {
+
+
+  if(player.x<0)
+  player.x =0;
+  if (player.y<0)
+  player.y=0;
+}, 1);
+
+var Draw = setInterval(function() {
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+rect.Draw(ctx);
+player.Draw(ctx);
+}, 33);
