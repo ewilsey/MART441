8ef6c9be600e533fede5ebe10c00295f87471a20
Eric Wilsey <ericwilsey@hotmail.com>
2019-05-05 20:51:58 -0600

    Added functionality to buttons using Jquery that would cycle pics forever

--- FINAL/scripts/main.js
+++ FINAL/scripts/main.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
-  $('.next').on('click',function(){
-    console.log('clicked');
+$(document).ready(function() {
+  $('.back').on('click', function(){
+    var backImg = $('img.active').prev('.slide-in img');
+    if(backImg.length == 0) {
+      backImg = $('.slide-in img:last');
+    }
+    $('img.active').removeClass('active');
+    backImg.addClass('active');
   });
-});
+  $('.next').on('click', function() {
+    var nextImg = $('img.active').next('.slide-in img');
+    if(nextImg.length == 0) {
+      nextImg = $('.slide-in img:first');
+    }
+    $('img.active').removeClass('active');
+    nextImg.addClass('active');
+  });
+});﻿
